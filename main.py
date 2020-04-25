969fd0dfb3b407f6b8c164f12bca50af63e4d3f0
Nastya Arkhipova <55373084+arhipovana2001@users.noreply.github.com>
2020-04-25 23:06:12 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+from rome import *
+
+
+num1 = Rome(input())
+num2 = Rome(input())
+#print('num1 =', num1)
+#print('num2 =', num2)
+#print(num1 + num2)
+#print(num1 - num2)
+#print(num1 * num2)
+#print(num1 / num2)
+#print(num1 == num2)
+#print(num1 != num2)
+#print(num1 > num2)
+#print(num1 < num2)
+#print(num1 >= num2)
+#print(num1 <= num2)
